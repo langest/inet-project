dc42d1294452739c1d97fb51f52fc901f7f6f1d0
dwalky <dkarlbom@kth.se>
2015-03-07 11:06:53 +0100

    Added right syntax

--- server.go
+++ server.go
@@ -61,7 +61,7 @@ func handleLogin(w http.ResponseWriter, r *http.Request) {
 			log.Println("logged in successfully")
 			session.Values["username"] = username
 			//TODO show successful login page and redirect to home or something
-			session.Save()
+			session.Save(r,w)
 		} //TODO else show unsuccessful and show login again
 		fmt.Fprintf(w, readFile("login.html"))
 	}
