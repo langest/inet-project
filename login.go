3172e40c3bfba98f77a28b9bcdfa6d0e4002ef99
Daniel Langesten <daniel.langest@gmail.com>
2015-03-07 10:10:43 +0100

    removed unused code

--- login.go
+++ login.go
@@ -14,26 +14,6 @@ var (
 	currentPassword = hashPassword("")
 )
 
-const (
-	PW_SALT_BYTES = 32
-	PW_HASH_BYTES = 64
-)
-
-func hashPassword(password string) string {
-	salt := make([]byte, PW_SALT_BYTES)
-	_, err := io.ReadFull(rand.Reader, salt)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	hash, err := scrypt.Key([]byte(password), salt, 1<<14, 8, 1, PW_HASH_BYTES)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return string(hash)
-}
-
 func NewUser(newUsername, newPassword string) {
 	currentUser = newUsername
 	currentPassword = newPassword
